1e38facc032aec0201bef485b012bf0eb485ca47
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-09 15:56:06 +0200

    created Table columns

--- database/migrations/2022_09_09_131833__create_trains_migration.php
+++ database/migrations/2022_09_09_131833__create_trains_migration.php
@@ -13,7 +13,17 @@ class CreateTrainsMigration extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('trains', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('company', 50);
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->string('train_code', 15);
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_delete')->default(0);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +33,6 @@ class CreateTrainsMigration extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('trains');
     }
 }
